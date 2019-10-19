2ffbef0c4f5b7182d7a78523e2c882e28a4d81c0
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2019-10-18 21:58:33 -0300

    Payload Book

---
+++ app/src/main/java/br/com/pedrofsn/kotlincoroutines/Book.kt
@@ -0,0 +1,10 @@
+package br.com.pedrofsn.kotlincoroutines
+
+data class Book(
+    val abbrev: String?, // ap
+    val author: String?, // João
+    val chapters: Int?, // 22
+    val group: String?, // Revelações
+    val name: String?, // Apocalipse
+    val testament: String? // NT
+)

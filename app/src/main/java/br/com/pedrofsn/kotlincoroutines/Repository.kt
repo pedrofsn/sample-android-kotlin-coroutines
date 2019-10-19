98503b6cf52b4de0cfd0add57fb6188c3f62bc95
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2019-10-18 22:49:48 -0300

    Handling error case

--- app/src/main/java/br/com/pedrofsn/kotlincoroutines/Repository.kt
+++ app/src/main/java/br/com/pedrofsn/kotlincoroutines/Repository.kt
@@ -1,10 +1,12 @@
 package br.com.pedrofsn.kotlincoroutines
 
 import okhttp3.OkHttpClient
+import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.net.UnknownHostException
 
-class Repository {
+class Repository(val onFailure: suspend (String) -> Unit) {
 
     private val service by lazy {
         val clientHttp = OkHttpClient().newBuilder().build()
@@ -19,8 +21,25 @@ class Repository {
         retrofit.create(API::class.java)
     }
 
-    suspend fun getBooks(): List<Book> {
-        return service.getBooks()
+    suspend fun getBooks() = try {
+        service.getBooks()
+    } catch (e: Exception) {
+        handleError(e)
+        emptyList<Book>()
+    }
+
+    private suspend fun handleError(e: Exception) {
+        when (e) {
+            is HttpException -> {
+                val error = "${e.code()} - ${e.message()}"
+                onFailure.invoke(error)
+            }
+            is UnknownHostException -> {
+                val error = "Verifique suas condições de internet\nToque para tentar novamente!"
+                onFailure.invoke(error)
+            }
+            else -> onFailure.invoke("veja bem -> ${e.message}")
+        }
     }
 
 }

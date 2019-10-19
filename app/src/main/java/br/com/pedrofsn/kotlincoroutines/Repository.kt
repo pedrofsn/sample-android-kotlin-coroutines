16ff7fd23e9aad8596a645202d6bd3b8408b7daf
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2019-10-18 21:59:31 -0300

    Repository pattern | It's not go horse, is just to study purposes

---
+++ app/src/main/java/br/com/pedrofsn/kotlincoroutines/Repository.kt
@@ -0,0 +1,22 @@
+package br.com.pedrofsn.kotlincoroutines
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class Repository {
+
+    private val service by lazy {
+        val clientHttp = OkHttpClient().newBuilder().build()
+        val converter = MoshiConverterFactory.create()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://bibleapi.co/api/")
+            .addConverterFactory(converter)
+            .client(clientHttp)
+            .build()
+
+        retrofit.create(API::class.java)
+    }
+
+}
